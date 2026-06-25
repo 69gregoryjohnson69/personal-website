53f39c73430a1365de30144761ffc28e52750c1a
Gregory Johnson <7gregory.johnson.7@gmail.com>
2026-06-25 15:46:17 -0500

    Fix Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gregoryjohnson.dev',
+  site: 'https://69gregoryjohnson69.github.io',
+  base: '/personal-website',
   build: {
     inlineStylesheets: 'auto',
   },
